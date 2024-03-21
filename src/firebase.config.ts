cf234b369ab37263fcb86ab71f0c22177db16466
Juan Pablo Pérez Lantes <jplantes@gmail.com>
2024-03-21 16:15:30 -0300

    Se configuran variables de entorno

---
+++ src/firebase.config.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_APP_APIKEY,
+  authDomain: import.meta.env.VITE_APP_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_APP_PROJECTID,
+  storageBucket: import.meta.env.VITE_APP_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APP_APPID,
+}
+
+// Initialize Firebase
+initializeApp(firebaseConfig)
